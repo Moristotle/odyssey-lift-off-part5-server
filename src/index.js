b1eb48c97dbcd678a7cc453b0d935bb2e64f1cd2
Mori Diakite <Mori.Diakite@inmeta.no>
2022-03-03 00:15:57 +0100

    fix: Added conditional reading of port in server setup

--- src/index.js
+++ src/index.js
@@ -1,25 +1,20 @@
-const { ApolloServer } = require('apollo-server');
-const typeDefs = require('./schema');
-const resolvers = require('./resolvers');
-const TrackAPI = require('./datasources/track-api');
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./schema");
+const resolvers = require("./resolvers");
+const TrackAPI = require("./datasources/track-api");
 
 async function startApolloServer(typeDefs, resolvers) {
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    dataSources: () => {
-      return {
-        trackAPI: new TrackAPI(),
-      };
-    },
-  });
+	const server = new ApolloServer({
+		typeDefs,
+		resolvers,
+		dataSources: () => {
+			return {
+				trackAPI: new TrackAPI(),
+			};
+		},
+	});
 
-  const { url, port } = await server.listen();
-  console.log(`
-      🚀  Server is running
-      🔉  Listening on port ${port}
-      📭  Query at ${url}
-    `);
+	await server.listen({ port: process.env.PORT || 4000 });
 }
 
 startApolloServer(typeDefs, resolvers);
